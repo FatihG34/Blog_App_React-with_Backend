d96aba7c065237c0ce0ab819efe5b8f4a6a520ef
FatihG34 <fthgnydn34@gmail.com>
2022-10-14 23:19:34 +0300

    starter files added and modified

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import AppRouter from './router/AppRouter';
 
 function App() {
   return (
-    <div className="App">
-      This is Blog App Project
+    <div>
+      <AppRouter />
     </div>
   );
 }
