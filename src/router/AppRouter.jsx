6cc9d0e92b18a0caad23c43e1236a70a0e477686
FatihG34 <fthgnydn34@gmail.com>
2022-10-15 20:11:45 +0300

    font added navbar modified

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from '../components/Navbar';
 import About from '../pages/About';
 import Dashboard from '../pages/Dashboard';
 import Login from '../pages/Login';
@@ -11,6 +12,7 @@ import UpdateBlog from '../pages/UpdateBlog';
 const AppRouter = () => {
     return (
         <BrowserRouter>
+            <Navbar />
             <Routes>
                 <Route path='/' element={<Dashboard />} />
                 <Route path='/about' element={<About />} />
