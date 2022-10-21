e4b4aaa5b44e8211dad0e5d980631ba5cfd05936
FatihG34 <fthgnydn34@gmail.com>
2022-10-21 10:25:35 +0300

    modified

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from '../components/navbar/Navbar';
 import About from '../pages/About';
+import BlogDetails from '../pages/BlogDetails';
 import Dashboard from '../pages/Dashboard';
 import Login from '../pages/Login';
 import NewBlog from '../pages/NewBlog';
@@ -20,6 +21,7 @@ const AppRouter = () => {
                 <Route path='/register' element={<Register />} />
                 <Route path='/login' element={<Login />} />
                 <Route path='/newblog' element={<NewBlog />} />
+                <Route path='/details/:str' element={<BlogDetails />} />
                 <Route path='/update' element={<UpdateBlog />} />
                 <Route path='/profile' element={<Profile />} />
             </Routes>
